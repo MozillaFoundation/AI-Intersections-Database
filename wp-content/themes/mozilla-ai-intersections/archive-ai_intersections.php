c19b3cd583c415044351962745aa0e736948c87f
Ramon Ramos <ramon@mozillafoundation.org>
2024-08-13 16:34:26 -0600

    Update archive template sanitizing query params

--- wp-content/themes/mozilla-ai-intersections/archive-ai_intersections.php
+++ wp-content/themes/mozilla-ai-intersections/archive-ai_intersections.php
@@ -21,7 +21,7 @@
                         <label for="search-toggle" class="font-header font-medium text-white text-[22px] leading-[1.3] whitespace-nowrap mr-4 lg:flex lg:items-center lg:justify-between lg:gap-6 lg:w-full lg:mr-0 sm:text-[20px]">Search the database<span class="lg:hidden">:</span></label>
 
                         <div class="database-search__container flex items-center justify-between gap-4 lg:hidden lg:flex-col lg:gap-0">
-                            <input id="search" type="text" name="s" placeholder="Find a social justice issue, AI impact, movement actor, or location"<?php if ( isset( $_GET['search'] ) ) echo ' value="' . $_GET['search'] . '"'; ?>>
+                            <input id="search" type="text" name="s" placeholder="Find a social justice issue, AI impact, movement actor, or location"<?php if ( isset( $_GET['search'] ) ) echo ' value="' . sanitize_text_field($_GET['search']) . '"'; ?>>
                             <input type="hidden" name="post_type" value="ai_intersections">
                             <input id="search-submit" type="submit" value="Search">
                         </div>
@@ -402,7 +402,7 @@
     
                             $args = array(
                                 'post_type' => 'ai_intersections',
-                                'posts_per_page' => isset( $_GET['records'] ) ? $_GET['records'] : 8,
+                                'posts_per_page' => isset( $_GET['records'] ) ? intval($_GET['records']) : 8,
                                 'orderby' => 'date',
                                 'order' => 'DESC',
                                 'tax_query' => $tax_query,
@@ -410,7 +410,8 @@
                             );
     
                             if ( isset( $_GET['sort'] ) ):
-                                switch ( $_GET['sort'] ):
+                                $sort = sanitize_text_field($_GET['sort']);
+                                switch ( $sort ):
                                     case 'recent':
                                         $args['orderby'] = 'date';
                                         $args['order'] = 'DESC';
